307a67abbbcc62cb448b41e479071d2c95bba5f1
YIYANG <feiyyfrank@gmail.com>
2020-12-25 03:20:02 +0000

    send private message

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from flask import Flask
-from flask_socketio import SocketIO, send
+from flask import Flask,request
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 
+rooms = {}
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('message')
 def handle_message(message):
-    print("Socket ID:");
+    print("Session ID:"+request.sid);
     print(message);
-    send(message, broadcast=True);
+    for room in rooms:
+        if request.sid in rooms[room]:
+            for user in rooms[room]:
+                print(room);
+                if user != request.sid:
+                    send(message, room=user);
+                    print(request.sid+" sent message: "+message['message']+" to "+user);
+
+
+@socketio.on('join')
+def handle_join(data):
+    room = [request.sid, data['roomId']];
+    rooms['room_'+data['roomId']]=room;
+    print(request.sid+" joins "+data['roomId']);
+    print(rooms);
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000);
